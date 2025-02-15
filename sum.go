41ae03a671d03d3575aea7e406f34a4d9e39dae8
alexandre <alexandrec658@gmail.com>
2025-02-15 12:08:20 -0300

    test: coverage func sum

--- sum.go
+++ sum.go
@@ -4,10 +4,15 @@ import "fmt"
 
 func main() {
 
-	fmt.Println(sum(5, 5))
+	result, err := sum(5, 5)
+	if err != nil {
+		fmt.Println("Error", err)
+	} else {
+		fmt.Println("Result", result)
+	}
 
 }
 
-func sum(a, b int) int {
-	return a + b
+func sum(a, b int) (int, error) {
+	return a + b, nil
 }
--- sum_test.go
+++ sum_test.go
@@ -8,10 +8,17 @@ func Test(t *testing.T) {
 
 	t.Run("Should calculate sum", func(t *testing.T) {
 
-		total := sum(5, 5)
+		total, err := sum(5, 5)
+
+		if err != nil {
+			t.Errorf("Error: %s", err)
+		}
+
 		if total != 10 {
 			t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
+
 		}
+
 	})
 
 }
