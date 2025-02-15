2925bdb7f97a18f784e719c9c7df971728091587
alexandre <alexandrec658@gmail.com>
2025-02-15 11:49:42 -0300

    test: coverage

--- sum.go
+++ sum.go
@@ -5,19 +5,9 @@ import "fmt"
 func main() {
 
 	fmt.Println(sum(5, 5))
-	fmt.Println(sub(5, 5))
-	fmt.Println(times(5, 5))
 
 }
 
 func sum(a, b int) int {
 	return a + b
-}
-
-func sub(a, b int) int {
-	return a - b
-}
-
-func times(a, b int) int {
-	return a * b
 }
--- sum_test.go
+++ sum_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -14,63 +13,5 @@ func Test(t *testing.T) {
 			t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
 		}
 	})
-
-	t.Run("Should calculate sub", func(t *testing.T) {
-
-		total := sub(5, 5)
-		if total != 0 {
-			t.Errorf("Sub was incorrect, got: %d, want: %d.", total, 0)
-		}
-	})
-
-	t.Run("Should calculate times", func(t *testing.T) {
-
-		total := times(5, 5)
-		if total != 25 {
-			t.Errorf("Times was incorrect, got: %d, want: %d.", total, 25)
-		}
-	})
-
-	t.Run("Should print sum", func(t *testing.T) {
-		
-		result := sum(5, 5)
-		_, err := fmt.Println(result)
-		
-		if err != nil {
-			t.Errorf("Error: %v", err)
-		}
-
-		if result != 10 {
-			t.Errorf("Sum was incorrect, got: %d, want: %d.", result, 10)
-		}
-	})
-
-	t.Run("Should print sub", func(t *testing.T) {
-		
-		result := sub(5, 5)
-		_, err := fmt.Println(result)
-		
-		if err != nil {
-			t.Errorf("Error: %v", err)
-		}
-
-		if result != 0 {
-			t.Errorf("Sub was incorrect, got: %d, want: %d.", result, 0)
-		}
-	})
-	t.Run("Should print times", func(t *testing.T) {
-		
-		result := times(5, 5)
-		_, err := fmt.Println(result)
-		
-		if err != nil {
-			t.Errorf("Error: %v", err)
-		}
-
-		if result != 25 {
-			t.Errorf("Times was incorrect, got: %d, want: %d.", result, 25)
-		}
-	})
-
 
 }
